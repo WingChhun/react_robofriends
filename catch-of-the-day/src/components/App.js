96a4fc62e1baf7f45b449ae18c52e5f40cddcc2a
WingChhun <capt.wing.chhun@gmail.com>
2018-03-15 12:16:41 -0700

    created state, and addFish function

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,32 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+  constructor() {
+    super(); //inherit React properties
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+  addFish = fish => {
+    console.log("Adding a fish...");
+    //1. take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    //2. Add our new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes object to state
+    this.setState({ fishes: fishes });
+    //Refresh the form
+    
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
