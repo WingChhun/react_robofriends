10ea43571c34f08408c84aa0ebe3e5e85c307776
WingChhun <capt.wing.chhun@gmail.com>
2018-03-15 13:36:34 -0700

    Update App.js , add loadSampleFishes function and Map over fish state object keys

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,7 +2,8 @@ import React from "react"; //import react
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
-
+import sampleFishes from "../sample-fishes"; //sample fishes
+import Fish from "./Fish";
 class App extends React.Component {
   constructor() {
     super(); //inherit React properties
@@ -11,6 +12,7 @@ class App extends React.Component {
       order: {}
     };
   }
+  //Update fishes in state
   addFish = fish => {
     console.log("Adding a fish...");
     //1. take a copy of the existing state
@@ -20,16 +22,28 @@ class App extends React.Component {
     //3. Set the new fishes object to state
     this.setState({ fishes: fishes });
     //Refresh the form
-    
+  };
+  //Create sample fishes
+  loadSampleFishes = () => {
+    console.log("Loading sample fishes");
+    this.setState({ fishes: sampleFishes });
   };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
