2f49a07b896cf3e99008c9003278cfedbdfed20e
WingChhun <chhunjamesdc2@yahoo.com>
2018-03-16 23:43:59 -0700

    Display order from firebase

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { formatPrice } from "../helpers";
+import {formatPrice} from "../helpers";
 class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key]; //get fishes prop to get th eprice
     const count = this.props.order[key];
-    const isAvailable = fish.status === "available";
+    const isAvailable = fish && fish.status === "available";
+    if (!fish) 
+      return null;
     if (!isAvailable) {
       return (
         <li key={key}>
-          Sorry {fish ? fish.name : "fish"} is no longer available
+          Sorry {fish
+            ? fish.name
+            : "fish"}
+          is no longer available
         </li>
       );
     }
     return (
       <li key={key}>
-        {count} lbs {fish.name}
+        {count}
+        lbs {fish.name}
         {formatPrice(count) * fish.price}
       </li>
     );
