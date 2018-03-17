9173fd1cf1e301131a1407d2fbc101919509eda0
WingChhun <capt.wing.chhun@gmail.com>
2018-03-16 18:52:06 -0700

    Complete Fish component

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { formatPrice } from "../helpers";
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
   render() {
     //destructure object
     const { image, name, price, desc, status } = this.props.details;
-
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -13,7 +16,10 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        {/*If unavialable, add disabled attribute*/}
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? "Add To Order" : "Sold Out!"}
+        </button>
       </li>
     );
   }
