fc19ad06fb5e3a3ccaadd02f2d9201e02f1323a0
WingChhun <chhunjamesdc2@yahoo.com>
2018-03-17 10:26:28 -0700

    Pass updateFishForm from props into edit, as well as key and index

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,14 +1,23 @@
 import React from "react";
 import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        <h2> Inventory</h2>
-        <AddFishForm addFish = {this.props.addFish}/>
-      <button onClick ={this.props.loadSampleFishes}>Load Sample Fishes</button>
+        <h2>
+          Inventory</h2>
+        {/*map over prop.fishes and render a EditFishForm*/}
+        {Object
+          .keys(this.props.fishes)
+          .map(key => <EditFishForm fish={this.props.fishes[key]} key={key} index = {key} updateFish = {this.props.updateFish}></EditFishForm>)}
+
+        <AddFishForm addFish={this.props.addFish}/>
+
+        <button onClick ={this.props.loadSampleFishes}>Load Sample Fishes</button>
+
       </div>
-    );
+    )
   }
 }
 
